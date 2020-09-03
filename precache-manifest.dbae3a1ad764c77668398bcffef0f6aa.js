c49d9627975da77b496fef379a5c68c1eeba8258
russellcxl <russellcxl@gmail.com>
2020-09-03 18:36:52 +0800

    Updates

---
+++ precache-manifest.dbae3a1ad764c77668398bcffef0f6aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5196a53f42cfe3261eec332dde4dd8",
+    "url": "/pipeline/index.html"
+  },
+  {
+    "revision": "94ba12edac84e1316041",
+    "url": "/pipeline/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6250e689dd3a21312ade",
+    "url": "/pipeline/static/css/main.27f64dcf.chunk.css"
+  },
+  {
+    "revision": "94ba12edac84e1316041",
+    "url": "/pipeline/static/js/2.aa984075.chunk.js"
+  },
+  {
+    "revision": "a05ff2ca95a280f2f63ee58bc9f23c36",
+    "url": "/pipeline/static/js/2.aa984075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6250e689dd3a21312ade",
+    "url": "/pipeline/static/js/main.098ae268.chunk.js"
+  },
+  {
+    "revision": "6392dc37335f57dfe772",
+    "url": "/pipeline/static/js/runtime-main.bca7d6d7.js"
+  }
+]);
