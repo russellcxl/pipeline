3ddf140e8edaea81c414feeb8c2a4bd105ba9572
russellcxl <russellcxl@gmail.com>
2020-09-04 09:22:51 +0800

    checking if frontend build adds

---
+++ frontend/build/precache-manifest.1903cb3d31bce35811a271157e91c208.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20123b053c54c3cd5ae2119ad74761f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "312d97eaff08db8424d7",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "94b6546dccfbec64db0a",
+    "url": "/static/css/main.5df91863.chunk.css"
+  },
+  {
+    "revision": "312d97eaff08db8424d7",
+    "url": "/static/js/2.590bfd14.chunk.js"
+  },
+  {
+    "revision": "a05ff2ca95a280f2f63ee58bc9f23c36",
+    "url": "/static/js/2.590bfd14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94b6546dccfbec64db0a",
+    "url": "/static/js/main.d6c17346.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
